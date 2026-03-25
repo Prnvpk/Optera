acdeaf6b45a368b2cafca08c62cfc3aeb5486d31
Pranav <pranavpk067@gmail.com>
2026-03-25 23:40:15 +0530

    Serve media files on deployed site

--- optera/urls.py
+++ optera/urls.py
@@ -53,5 +53,4 @@ urlpatterns = [
     path('my-orders/', views.user_order_status, name='user_order_status'),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
